f6c4ac3879f6f5a77f6fe869bb237f282b6945ac
rawiphon <37574@bpk.ac.th>
2026-09-10 22:26:14 +0700

    Update GitHub link in task2.php

--- task2.php
+++ task2.php
@@ -47,7 +47,7 @@ if (!$conn->connect_error) {
         </div>
         <div>
             <!-- ปุ่ม View Source ลิงก์ไปยัง GitHub แท็บใหม่ -->
-            <a href="https://github.com/YOUR_USERNAME/YOUR_REPOSITORY" target="_blank" class="btn btn-outline-dark">
+            <a href="https://github.com/37574-create/608/blob/main/task2.php"_blank" class="btn btn-outline-dark">
                 View Source on GitHub
             </a>
         </div>
